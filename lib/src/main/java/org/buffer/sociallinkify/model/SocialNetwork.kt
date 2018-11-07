acf9fe72d4860c6cea9437d62cd5769b2e71348c
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-07 08:17:46 +0000

    Add missing social networks for Buffer Publish

--- lib/src/main/java/org/buffer/sociallinkify/model/SocialNetwork.kt
+++ lib/src/main/java/org/buffer/sociallinkify/model/SocialNetwork.kt
@@ -3,13 +3,17 @@ package org.buffer.sociallinkify.model
 enum class SocialNetwork(val id: String) {
     TWITTER("twitter"),
     FACEBOOK("facebook"),
-    INSTAGRAM("instagram");
+    INSTAGRAM("instagram"),
+    LINKEDIN("linkedin"),
+    GOOGLE_PLUS("google");
 
     companion object {
         fun fromString(name: String) = when (name) {
             TWITTER.id -> TWITTER
             FACEBOOK.id -> FACEBOOK
             INSTAGRAM.id -> INSTAGRAM
+            LINKEDIN.id -> LINKEDIN
+            GOOGLE_PLUS.id -> GOOGLE_PLUS
             else -> throw IllegalArgumentException("Whoops, the social network $name isn't known!")
         }
     }
