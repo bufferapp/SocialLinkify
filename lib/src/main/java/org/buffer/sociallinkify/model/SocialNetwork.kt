3c769715b2cf7a445254e7219d19e482c4f5d85a
Joe Birch <hitherejoebirch@googlemail.com>
2018-12-04 10:11:29 +0000

    Add pinterest to network types

--- lib/src/main/java/org/buffer/sociallinkify/model/SocialNetwork.kt
+++ lib/src/main/java/org/buffer/sociallinkify/model/SocialNetwork.kt
@@ -5,6 +5,7 @@ enum class SocialNetwork(val id: String) {
     FACEBOOK("facebook"),
     INSTAGRAM("instagram"),
     LINKEDIN("linkedin"),
+    PINTEREST("pinterest"),
     GOOGLE_PLUS("google");
 
     companion object {
@@ -13,6 +14,7 @@ enum class SocialNetwork(val id: String) {
             FACEBOOK.id -> FACEBOOK
             INSTAGRAM.id -> INSTAGRAM
             LINKEDIN.id -> LINKEDIN
+            PINTEREST.id -> PINTEREST
             GOOGLE_PLUS.id -> GOOGLE_PLUS
             else -> throw IllegalArgumentException("Whoops, the social network $name isn't known!")
         }
